106bfaa8ff1e94d334dbcb9cfc168956568c965d
Oluwatoyin <105533960+Shortiii@users.noreply.github.com>
2026-03-27 11:57:37 +0100

    fix: add vercel routing

--- dev-latern/src/pages/codeChallenge.vue
+++ dev-latern/src/pages/codeChallenge.vue
@@ -1,3 +1,33 @@
 <template>
-  
+  <div>
+    <section class="relative w-full min-h-[520px] flex items-center justify-center overflow-hidden">
+
+      <img
+        src="../assets/hero-header.png"
+        alt=""
+        aria-hidden="true"
+        class="absolute inset-0 w-full h-70% object-cover z-0"
+      />
+
+      <div class="absolute inset-0 bg-[#04064699]"></div>
+
+      <div class="relative z-10 text-center px-6 py-20 max-w-2xl mx-auto">
+        <h1 class="text-3xl sm:text-4xl md:text-7xl font-medium text-white">
+          Code <span class="text-[#D3A52A]">Challenge</span>
+        </h1>
+      </div>
+    </section>
+
+    <section class="bg-[#F5F8FF]  py-20  ">
+
+        <div class="max-w-6xl mx-auto pb-12">
+            <h2 class="text-5xl text-[#1F357A] font-medium">Code Challenge</h2>
+            <p class="text-xl text-[#525252] pt-12">In August 2025, we launched the Dev Lantern Code Challenge to push learning beyond theory and into execution. Designed as a hands-on, problem-solving initiative, the Code Challenge encourages collaboration, strenghtens technical competence, and identifies emerging talent with the community. Dev Lantern, in collaboration with Talenvo is where theory meets execution. We host hands-on competitions and real-world problem-solving sessions that push participants beyond comfortcand into mastery, strenghtening practical skills, encouraging collaboration, and spotlight emerging talent. This is where builders prove what they know.</p>
+        </div>
+
+        <div class="max-w-6xl mx-auto bg-white p-4 ">
+
+        </div>
+    </section>
+  </div>
 </template>
